dd50e6b203307be699f0125958ab6dec8c8b3608
Ana María Cárdenas Gasca <am.cardenas927@gmail.com>
2018-07-17 12:06:29 -0500

    Refactor name of js modules

---
+++ tetris/frontend/js/modules/game_loader.js
@@ -0,0 +1,123 @@
+import * as FetchService from './fetch_service.js';
+
+let loadElement;
+let waitRendered;
+
+const loadGame = (le) => {
+    loadElement = le;
+    waitRendered = false;
+    const hash = new URL(window.location.href).hash.substring(1);
+    try {
+        const pData = hash ? JSON.parse(decodeURIComponent(hash)) : null;
+        const pToken = pData ? pData.playerToken : null;
+        if (pToken) {
+            FetchService.setToken(pToken);
+            const gameId = pData.gameId;
+            if (gameId) {
+                return pollGameState(gameId);
+            } else {
+                return FetchService.postData('game', {})
+                .then(
+                    gameStatus => pollGameState(gameStatus.gameId)
+                );
+            }
+        } else {
+            window.location.replace(`../..`);
+        }
+    }
+    catch(error) {
+        window.location.replace(`../..`);
+    }
+}
+
+const pollGameState = (gameId) => {
+    return FetchService.getData(`game/${gameId}`).then(gameStatus => {
+        if (gameStatus.active) {
+            if (gameStatus.isCurrentPlayer && !gameStatus.playerSymbol) {
+                loadSymbols(gameId)
+            } else if ((!gameStatus.isCurrentPlayer && !gameStatus.playerSymbol) || !gameStatus.playerTwo) {
+                renderWaiting();
+                pollGameState(gameId);
+            } else {
+                playGame(gameId);
+            }
+        }
+        else {
+            throw new InvalidStartState('cannot start a game that ended');
+        }
+    })
+}
+
+const renderWaiting = () => {
+    if (!waitRendered) {
+        console.log('waiting')
+        const container = document.createElement("div");
+        container.className = 'progress';
+        const loader = document.createElement("div");
+        loader.className = 'indeterminate';
+        container.appendChild(loader);
+        loadElement.innerHTML = 'Esperando a contrincante';
+        loadElement.appendChild(container);
+        waitRendered = true;
+    }
+}
+const playGame = (gameId) => {
+    const playerToken = FetchService.getToken();
+    const playerData = { gameId, playerToken };
+    const pEndodedData = encodeURIComponent(JSON.stringify(playerData));
+    window.location.replace(`./play_game.html#${pEndodedData}`);
+}
+
+const loadSymbols = (gameId) => {
+    FetchService.getData(`symbol/${gameId}`).then(symbols => {
+        const title = document.createElement("span");
+        title.innerHTML = 'Seleccione un simbolo';
+        loadElement.appendChild(title);
+        for (const key in symbols) { 
+            const p =  document.createElement("p");
+            const label = document.createElement("label");
+            const input = document.createElement("input");
+            input.type = 'radio';
+            input.name = `group-1`; 
+            const span = document.createElement("span");
+            span.innerHTML = symbols[key];
+            label.appendChild(input);
+            label.appendChild(span);
+            p.appendChild(label);
+            loadElement.appendChild(p);
+        }
+        const button = createSelectButton(gameId);
+        loadElement.appendChild(button);
+    })
+}
+
+const createSelectButton = (gameId) => {
+    const selectButton = document.createElement("a");
+    selectButton.className = 'waves-effect waves-light btn symbol-select';
+    selectButton.onclick = () => {
+        const selectedItem = Array.from(document.getElementsByTagName('input')).find(i => i.checked);
+        if (selectedItem) {
+            selectSymbol(gameId, selectedItem.nextElementSibling.innerText);
+        } else {
+            console.log('mostrar error');
+        }
+    }
+    selectButton.innerHTML = 'Seleccionar';
+    const icon = document.createElement("i");
+    icon.className = 'material-icons left';
+    icon.innerHTML = 'star_border';
+    selectButton.appendChild(icon);
+    return selectButton;
+}
+
+const selectSymbol = (gameId, symbol) => {
+    FetchService.putData(`set_symbol/${gameId}`, {symbol}).then(() => {
+        return pollGameState(gameId);
+    })
+}
+
+class InvalidStartState extends Error {
+}
+
+
+export {loadGame, selectSymbol, InvalidStartState}
--- tetris/frontend/js/tictactoe.js
+++ tetris/frontend/js/tictactoe.js
@@ -1,12 +1,17 @@
 import * as Player from './modules/player.js';
-import * as Game from './modules/app_game.js';
+import * as GameLoader from './modules/game_loader.js';
+import * as AppGame from './modules/app_game.js';
 
 function registerPlayer(playerName) {
     Player.registerPlayer(playerName);
 }
 
-function startGame(selectElement) {
-    Game.loadGame(selectElement)
+function loadGame(loadElement) {
+    GameLoader.loadGame(loadElement);
 }
 
-export {registerPlayer, startGame};
+function startGame(gameElement) {
+    AppGame.startGame(gameElement);
+}
+
+export {registerPlayer, loadGame, startGame};
